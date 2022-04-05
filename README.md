fd061459df92a51bb5f2beb7159f1b7c38863da7
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-05 10:37:47 +0200

    added responsive pages

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# ⚛️ Template for react
-
-This repo allows to create a responsive react project with a navbar and a footer with just one click.
-
-<img width="1188" alt="Schermata 2022-03-28 alle 16 58 22" src="https://user-images.githubusercontent.com/88108002/160426997-bd64c164-bc0f-4d0f-be9f-010c84ba5342.png">
-
-To run the app you should have installed node on your terminal, after this change the directory and copy the code below
-
-```bash
-npm start
-```
+Navbar using React-bootstrap.
--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComp from "./Components/NavbarComp";
 import Footer from "./Components/Footers";
-import React, { Component } from "react";
-import MyNavbar from "./Components/Navbar";
-//Non rappresenta documenti dinamicamente
-//function App() {
 
-//Funziona in modo dinamico e mi permette di mettere i props
-class App extends Component {
-  render() {
-    return (
-      <div classNameName="App">
-        <MyNavbar></MyNavbar>
-        <div className="content">This is a content</div>
-        <Footer></Footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <NavbarComp />
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
