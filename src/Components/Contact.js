3efe82707cf7beccd05f09dd45b6ba0a9248f35f
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-11 13:40:41 +0200

    now the form works

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,47 +1,72 @@
-import React, { Component } from "react";
-import emailjs from "@emailjs/browser";
+import { useEffect, useState } from "react";
 import { useRef } from "react";
-import Form from "react-bootstrap/Form";
+import emailjs from "@emailjs/browser";
 
-export default class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Contact</h2>
-        <form>
-          <ul>
-            <div className="Padding">
-              <input placeholder="Name" type="text" name="name" required />
-            </div>
-            <div className="Padding">
-              <input placeholder="Email" type="email" name="email" required />
-            </div>
+const Contact = () => {
+  const form = useRef();
 
-            <div className="Padding">
-              {" "}
-              <input
-                placeholder="Subject"
-                type="text"
-                name="subject"
-                required
-              />
-            </div>
-            <div className="Padding">
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_mrk0h4p",
+        "template_x5bmvhc",
+        form.current,
+        "nW6LUTi3pEck0RPd-"
+      )
+      //emailjs
+      // .sendForm("service_ls6d3ox", "template_x5bmvhc")
+
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload(false);
+        },
+        () => {
+          alert("Failed to send the message, please try again");
+        }
+      );
+  };
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="Padding_top">
+                <input placeholder="Name" type="text" name="name" required />
+              </div>
+              <div className="Padding">
+                <input placeholder="Email" type="email" name="email" required />
+              </div>
+              <div className="Padding">
+                <input
+                  placeholder="Subject"
+                  type="text"
+                  name="subject"
+                  required
+                />
+              </div>
+
               <textarea
                 placeholder="Message"
                 name="message"
                 required
               ></textarea>
-            </div>
 
-            <input
-              type="submit"
-              className="btn btn-primary"
-              value="SEND"
-            ></input>
-          </ul>
-        </form>
+              <div className="Padding">
+                <button type="submit" class="btn btn-dark">
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
+
+export default Contact;
